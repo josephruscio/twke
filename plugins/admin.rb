c6a1ac89e0fbe4af578c6ec2092f623507b082cc
Mike Heffner <mikeh@fesnel.com>
2011-09-29 12:43:03 -0400

    Support 'admin join <roomname>' to join a new room after startup.

--- plugins/admin.rb
+++ plugins/admin.rb
@@ -8,6 +8,22 @@ class Plugin::Admin < Plugin
           Twke::shutdown
         end
       end
+
+      rp.route /join (?<room>.+)$/ do
+        # Convert to an integer if possible
+        roomname = room
+        begin
+          id = Integer(roomname)
+        rescue
+          id = rp.cmd { room_id_from_room_name(roomname) }
+        end
+
+        if !id
+          say "No room by the name/ID: #{roomname}"
+        else
+          rp.cmd { join(id) }
+        end
+      end
     end
   end
 end
