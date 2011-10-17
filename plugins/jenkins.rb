c1dca58abe82629134f9dc5e3f78f8ae90578b9c
Joseph Ruscio <joe@ruscio.org>
2011-10-17 11:26:48 -0700

    jenkins plugin recognizes hyphenated project names

--- plugins/jenkins.rb
+++ plugins/jenkins.rb
@@ -1,10 +1,10 @@
 class Plugin::Jenkins < Plugin
   def add_routes(rp, opts)
-    rp.route /^\w+ #\d+ ".*?": (FAILURE|UNSTABLE) \(.*?\)$/, :noprefix => true do |act|
+    rp.route /^[\w-]+ #\d+ ".*?": (FAILURE|UNSTABLE) \(.*?\)$/, :noprefix => true do |act|
       act.play "drama"
     end
 
-    rp.route /^\w+ #\d+ ".*?": success$/, :noprefix => true do |act|
+    rp.route /^[\w-]+ #\d+ ".*?": success$/, :noprefix => true do |act|
       act.play "yeah"
     end
   end
