911ad08a1b829badd69f4cdf2aa45523d8a93ffb
Mike Heffner <mikeh@fesnel.com>
2011-11-07 22:42:34 -0500

    Refactor plugin registration and event handling. Allow external plugins to be loaded at runtime with the `admin.load_plugins.*` configurable.

--- plugins/admin.rb
+++ plugins/admin.rb
@@ -33,6 +33,7 @@ class Plugin::Admin < Plugin
 
   # On connection
   def on_connect(rp, opts)
+    load_plugins(rp, opts)
     join_rooms(rp, opts)
   end
 
@@ -65,6 +66,59 @@ private
         join_room(rp, id)
       else
         puts "ERROR: Unable to find autojoin room #{room}(id: #{id})"
+      end
+    end
+  end
+
+  def load_plugins(rp, opts)
+    plugins = Twke::Conf.list('admin.load_plugins')
+    plugins.each do |plugin|
+      repo = Twke::Conf.get("admin.load_plugins.#{plugin}.repo")
+      path = Twke::Conf.get("admin.load_plugins.#{plugin}.path")
+
+      unless repo || path
+        puts "ERROR: Plugin %s does not have a repo or path set" % [plugin]
+        next
+      end
+
+      if repo
+        # Dir is a relative directory under the plugin to look for
+        # plugins from.
+        #
+        dir = Twke::Conf.get("admin.load_plugins.#{plugin}.dir")
+        dir = "plugins" unless dir
+
+        clone_plugin_repo(repo, dir)
+      else
+        load_plugin_dir(path, ".")
+      end
+    end
+  end
+
+  def clone_plugin_repo(repo, dir)
+    tmpdir = ENV['TMPDIR'] || "/tmp"
+    rdir = "#{tmpdir}/repo_#{rand 999999}"
+
+    d = Twke::JobManager.spawn("git clone -q #{repo} #{rdir}")
+    d.errback do
+      puts "Failed to clone plugin repo #{repo} to #{rdir}"
+    end
+
+    d.callback do
+      # Load plugins from the repo.
+      #
+      # TODO: Provide an after-checkout method to perform setup
+      # steps for the repo (e.g., bundle install)
+      #
+      load_plugin_dir(rdir, dir)
+    end
+  end
+
+  def load_plugin_dir(rdir, dir)
+    Dir["#{rdir}/#{dir}/*.rb"].each do |plugin|
+      puts "Loading plugin #{plugin}"
+      EM::next_tick do
+        load plugin
       end
     end
   end
