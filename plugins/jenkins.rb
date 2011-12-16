39a7d5898332b0960093dd9ca29a7558db17f5a1
Mike Heffner <mikeh@fesnel.com>
2011-12-16 01:41:46 -0500

    Literal hyphens should always go at the beginning of a bracket set.

--- plugins/jenkins.rb
+++ plugins/jenkins.rb
@@ -3,13 +3,13 @@ class Plugin::Jenkins < Plugin
   DefaultFail = 'drama'
 
   def add_routes(rp, opts)
-    rp.route /^[\w-.]+ #\d+ ".*?": (FAILURE|UNSTABLE) \(.*?\)$/, :noprefix => true do |act|
+    rp.route /^[-\w.]+ #\d+ ".*?": (FAILURE|UNSTABLE) \(.*?\)$/, :noprefix => true do |act|
       fail_sound = Twke::Conf.get('jenkins.sounds.fail')
 
       act.play (fail_sound) ? fail_sound : DefaultFail
     end
 
-    rp.route /^[\w-.]+ #\d+ ".*?": success$/, :noprefix => true do |act|
+    rp.route /^[-\w.]+ #\d+ ".*?": success$/, :noprefix => true do |act|
       success_sound = Twke::Conf.get('jenkins.sounds.success')
 
       act.play (success_sound) ? success_sound : DefaultSuccess
